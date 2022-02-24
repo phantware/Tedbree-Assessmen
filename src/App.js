297c2571e8022fd11c20f6873f7d82ca06dc745b
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-24 12:21:15 +0100

    Navbar component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Navbar from './navbar/Navbar'
 
 function App() {
-  return <div>Hello React</div>
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
 }
 
 export default App
