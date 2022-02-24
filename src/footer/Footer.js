ae4f3ea40a34c1f7b889ca0749fcce48e1c6134b
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-24 20:46:28 +0100

    footer component created

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './footer.css'
 
 const Footer = () => {
   return (
@@ -9,9 +10,33 @@ const Footer = () => {
           alt=''
           className='footer-logo'
         />
+        <h3 className='copywright'>
+          &copy; 2021 <span className='find-job'>FindJobs</span>
+        </h3>
       </div>
-      <div className='centerFooter'></div>
-      <div className='leftFooter'></div>
+      <div className='centerFooter'>
+        <h2 className='qlink'>Quick Links</h2>
+        <ul className='footer-ul'>
+          <li className='footer-li'>Home</li>
+          <li className='footer-li'>About</li>
+          <li className='footer-li'>Calender</li>
+          <li className='footer-li'>Terms and conditions</li>
+        </ul>
+      </div>
+      <div className='leftFooter'>
+        <h2 className='social-media'>Social Media</h2>
+        <ul className='social-ul'>
+          <li className='social-li'>
+            <i className='fab fa-instagram icon'></i>
+          </li>
+          <li className='social-li'>
+            <i className='fab fa-facebook icon'></i>
+          </li>
+          <li className='social-li'>
+            <i className='fab fa-twitter icon'></i>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
