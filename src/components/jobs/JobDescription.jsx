8a1e464412aec5122e58c4941ed44573b6553544
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-27 17:38:41 +0100

    jd logics created

--- src/components/jobs/JobDescription.jsx
+++ src/components/jobs/JobDescription.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import './jobdescription.css'
 
-const JobDescription = ({ data }) => {
+const JobDescription = ({ data, setId, setDisplay }) => {
+  const { id, title, price, location, description } = data
+
+  const setShow = (id) => {
+    setId(id)
+    setDisplay(true)
+  }
   return (
     <div className='jd-main'>
       <div className='jd-container'>
         <div className='jd-header'>
-          <h1 className='jd-desc'>Front end developer</h1>
-          <h3 className='jd-salary'>$2k-$5k</h3>
+          <h1 className='jd-desc'>{title}</h1>
+          <h3 className='jd-salary'>{price}</h3>
         </div>
         <div className='jd-location'>
           <span>
             <i className='fas fa-map-marker-alt icon2' />
           </span>
-          <span className='jd-locations'>Ikeja, Lagos</span>
+          <span className='jd-locations'>{location}</span>
         </div>
-        <h4 className='jd-detail'>
-          We seek an experienced Javascript web application developer who is
-          proficient with React.js and has excellent communication skills.
-        </h4>
-        <button className='jd-btn'>See more</button>
+        <h4 className='jd-detail'>{description}</h4>
+        <button className='jd-btn' onClick={() => setShow(id)}>
+          See more
+        </button>
       </div>
     </div>
   )
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,18 +3,22 @@ import JobDescription from '../../components/jobs/JobDescription'
 import './home.css'
 import { data } from '../../data'
 import JobSeeker from '../../components/jobs/JobSeeker'
-console.log('here is data', data)
+import { useState } from 'react'
 
 const Home = () => {
+  const [display, setDisplay] = useState(false)
+  const [id, setId] = useState('')
   return (
     <div className='home'>
       <div className='home-left'>
         {data.map((jd) => {
-          return <JobDescription data={jd} />
+          return (
+            <JobDescription data={jd} setId={setId} setDisplay={setDisplay} />
+          )
         })}
       </div>
       <div className='home-right'>
-        <JobSeeker />
+        <JobSeeker id={id} display={display} />
       </div>
     </div>
   )
