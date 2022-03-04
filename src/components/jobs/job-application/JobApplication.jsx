dbefe8d77247e92428aec20127d1006d3bdbffb9
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-03-04 09:22:50 +0100

    apply form created

--- src/components/jobs/job-application/JobApplication.jsx
+++ src/components/jobs/job-application/JobApplication.jsx
@@ -1,39 +1,65 @@
 import React from 'react'
+import './jobapplication.css'
 
 const JobApplication = () => {
   return (
     <div className='main'>
-      <h1 className='desc'>Front end developer</h1>
+      <div className='close-btn'>
+        <i class='fa fa-times close-icon' aria-hidden='true' />
+      </div>
+      <h1 className='job-desc'>Front end developer</h1>
       <div className='locations'>
-        <span className='cv-icon'>icon</span>
-        <span className='location'>icon</span>
+        <span className='cv-icon'>
+          <i className='fas fa-map-marker-alt icon2' />
+        </span>
+        <span className='location'>Ikeja Lagos</span>
       </div>
       <form className='job-form'>
         <div className='form-input'>
           <p>First Name</p>
-          <input type='text' />
+          <input type='text' className='form-inputs' />
         </div>
         <div className='form-input'>
           <p>Last Name</p>
-          <input type='text' />
+          <input type='text' className='form-inputs' />
         </div>
         <div className='form-input'>
           <p>Email Address</p>
-          <input type='email' />
+          <input type='email' className='form-inputs' />
         </div>
         <div className='form-input'>
           <p>Location</p>
-          <input type='text' />
+          <input type='text' className='form-inputs' />
         </div>
         <div className='form-input'>
           <p>Phone Number</p>
-          <input type='text' />
+          <input type='text' className='form-inputs' />
         </div>
         <div className='upload-cv'>
-          <div className='upload-cv-logo'>upload cv logo</div>
+          <div className='logo-img'>
+            <img
+              src='/assets/images/Vector2.svg'
+              alt='cv-logo'
+              className='upload-cv-logo2'
+            />
+            <img
+              src='/assets/images/Vector1.svg'
+              alt='cv-logo'
+              className='upload-cv-logo1'
+            />
+          </div>
           <h3 className='drag-drop'>Drag and drop your CV here</h3>
+          <h3 className='or'>or</h3>
           <div className='browse'>
-            <input type='file' name='cv' id='cv' className='file' />
+            <label htmlFor='fileInput'>
+              <div className='browseFile'>Browse file</div>
+            </label>
+            <input
+              type='file'
+              name='cv'
+              id='fileInput'
+              style={{ display: 'none' }}
+            />
           </div>
         </div>
         <input
