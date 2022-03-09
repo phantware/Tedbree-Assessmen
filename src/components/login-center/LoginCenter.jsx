085412c76b40acdd58321c66d7c852d7737b3e45
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-03-09 13:33:48 +0100

    logincenter component styled

--- src/components/login-center/LoginCenter.jsx
+++ src/components/login-center/LoginCenter.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import './logincenter.css'
 
 const LoginCenter = () => {
-  return <div>LoginCenter</div>
+  return (
+    <div className='logincenter-main'>
+      <div className='rectangle'></div>
+    </div>
+  )
 }
 
 export default LoginCenter
